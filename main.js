79a97e8e7b15b31398f5b22d86d232707c1572a1
Nolan Tremelling <34580718+NolanTrem@users.noreply.github.com>
2023-12-04 12:51:32 -0500

    More nav debugging

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     fetch('nav.html')
-        .then(response => response.text())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
         .then(data => {
             document.querySelector('nav').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
         });
 });
